f6f7f20a62910a854c51110fdfb0473e2f2e44a7
Rumaan <rumaankalander@gmail.com>
2019-01-22 22:09:02 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(HomePage());
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        accentColor: Colors.deepOrangeAccent,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Custom Shapes'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: CustomPaint(
+            painter: ShapesPainter(),
+            child: Container(
+              height: 700,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ShapesPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    // set the paint color to be white
+    paint.color = Colors.white;
+
+    // Create a rectangle with size and width same as the canvas
+    var rect = Rect.fromLTWH(0, 0, size.width, size.height);
+
+    // draw the rectangle using the paint
+    canvas.drawRect(rect, paint);
+
+    paint.color = Colors.yellow;
+
+    // create a path
+    var path = Path();
+    path.lineTo(0, size.height);
+    path.lineTo(size.width, 0);
+    // close the path to form a bounded shape
+    path.close();
+
+    canvas.drawPath(path, paint);
+
+    // set the color property of the paint
+    paint.color = Colors.deepOrange;
+
+    // center of the canvas is (x,y) => (width/2, height/2)
+    var center = Offset(size.width / 2, size.height / 2);
+
+    // draw the circle with center having radius 75.0
+    canvas.drawCircle(center, 75.0, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
